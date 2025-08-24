239cfacf2d1a221def9f07021370d993e55a6008
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2025-08-24 13:28:53 +0300

    Demonstrates string ownership in Rust
    Improves understanding of string ownership by moving a string.
    Explains that `s1` is no longer valid after moving its ownership to `s2`.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let s1 = String::from("hello");
-    take_ownership(s1);
+    let s1 = String::from("Rust Rules!");
+    let s2 = s1; // s1 is moved to 
+    println!("{}", s2); // s1 is no longer valid since the ownership has moved to s2
 }
 
-fn take_ownership(string: String) {
-    println!("{} this is from take ownership fn.", string);
-}
